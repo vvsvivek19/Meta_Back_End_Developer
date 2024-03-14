ad025083194e664a451b10384a8a9c5b3cbaad54
Vivek Singh <vvsvivek19@gmail.com>
2024-03-15 04:10:51 +0530

    Testing

--- 1. Programming in Python/tempCodeRunnerFile.py
+++ 1. Programming in Python/tempCodeRunnerFile.py
@@ -1,13 +1,16 @@
-a = 5
 class A:
-      a = 7
-      pass
+   def a(self):
+       return "Function inside A"
 
-class B(A):
-      pass
+class B:
+   def a(self):
+       return "Function inside B"
 
-class C(B):
-      pass
+class C:
+   pass
 
-c = C()
-print(c.a)
+class D(C, A, B):
+   pass
+
+d = D()
+print(d.a())
